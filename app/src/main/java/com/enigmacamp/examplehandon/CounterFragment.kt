0b14fb5f8a25ea5d52b865e8dc0fb759fefbc6d8
doni <donioctavian1016@gmail.com>
2021-07-08 09:34:44 +0700

    tictactoe

---
+++ app/src/main/java/com/enigmacamp/examplehandon/CounterFragment.kt
@@ -0,0 +1,41 @@
+package com.enigmacamp.examplehandon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_counter.*
+
+
+class CounterFragment : Fragment() {
+    private var showCounter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    //Proses set view
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_counter, container, false)
+    }
+
+    //Setelah semua berjalan, logic coding taruh di sini
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+
+        increaseButton.setOnClickListener {
+            showCounter++
+            activity.controllerData(showCounter)
+        }
+        decreaseButton.setOnClickListener {
+            showCounter--
+            activity.controllerData(showCounter)
+        }
+    }
+
+
+}
--- app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
@@ -2,10 +2,15 @@ package com.enigmacamp.examplehandon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_show.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun controllerData(value: Int) {
+        //ini id dari fragment 2 yang text_view
+        counterValue.text = value.toString()
     }
 }
