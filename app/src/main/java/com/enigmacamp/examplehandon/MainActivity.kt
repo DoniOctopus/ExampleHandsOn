6df750f0d32b75da26e406179b0ebae64400dc15
doni <donioctavian1016@gmail.com>
2021-07-08 09:19:58 +0700

    inputName

--- app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
@@ -2,10 +2,24 @@ package com.enigmacamp.examplehandon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textInputName: TextInputEditText
+    private lateinit var tvName: MaterialTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textInputName = findViewById(R.id.text_input_name)
+        tvName = findViewById(R.id.tv_name)
+    }
+
+    fun showName(view: View) {
+        val name = textInputName.text
+        tvName.text = name
     }
 }
