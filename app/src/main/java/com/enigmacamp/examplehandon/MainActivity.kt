fbf46799f64cfa2b51a129f9980916e7a06aecd1
doni <donioctavian1016@gmail.com>
2021-07-08 09:14:31 +0700

    master

---
+++ app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enigmacamp.examplehandon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
