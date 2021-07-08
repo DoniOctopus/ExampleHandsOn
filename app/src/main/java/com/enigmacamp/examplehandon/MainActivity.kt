4268b575a3c7b1d36752e072ae4a019209bd6ea5
doni <donioctavian1016@gmail.com>
2021-07-08 09:22:23 +0700

    counter

--- app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
@@ -2,10 +2,28 @@ package com.enigmacamp.examplehandon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvCounter: MaterialTextView
+    private var startCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCounter = findViewById(R.id.tv_counter)
     }
+
+    fun notifyIncrease(view: View) {
+        startCounter+=1
+        tvCounter.text = startCounter.toString()
+    }
+    fun notifyDecrease(view: View) {
+        startCounter-=1
+        tvCounter.text = startCounter.toString()
+    }
+
 }
