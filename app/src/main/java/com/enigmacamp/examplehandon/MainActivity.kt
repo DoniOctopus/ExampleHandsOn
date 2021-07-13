47a41a3915ccdf59f223ca6010977021ee14a4c9
doni <donioctavian1016@gmail.com>
2021-07-13 09:50:09 +0700

    InputName-Binding

--- app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/examplehandon/MainActivity.kt
@@ -3,23 +3,24 @@ package com.enigmacamp.examplehandon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.enigmacamp.examplehandon.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textInputName: TextInputEditText
-    private lateinit var tvName: MaterialTextView
+    private lateinit var viewBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textInputName = findViewById(R.id.text_input_name)
-        tvName = findViewById(R.id.tv_name)
-    }
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        viewBinding.apply {
+            btnShowName.setOnClickListener {
+                val name = textInputName.text
+                tvName.text = name
+            }
+        }
 
-    fun showName(view: View) {
-        val name = textInputName.text
-        tvName.text = name
     }
 }
