7b8339c38b40423fd0b7d20e16d1f19f1a386da5
doni <donioctavian1016@gmail.com>
2021-07-22 21:46:07 +0700

    navigation

---
+++ app/src/main/java/com/enigmacamp/examplehandon/SplashScreenFragment.kt
@@ -0,0 +1,37 @@
+package com.enigmacamp.examplehandon
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            Navigation.findNavController(view).navigate(R.id.action_splashScreenFragment_to_loginFragment)
+        }, 3000)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = SplashScreenFragment()
+    }
+}
