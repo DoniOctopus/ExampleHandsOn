b0eedbde9e874a52b39d39e1037d8219b35a9a21
doni <donioctavian1016@gmail.com>
2021-07-25 17:55:24 +0700

    navigation Item

--- app/src/main/java/com/enigmacamp/examplehandon/HomeFragment.kt
+++ app/src/main/java/com/enigmacamp/examplehandon/HomeFragment.kt
@@ -7,10 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_balance.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_transaction.*
 
 class HomeFragment : Fragment() {
 
@@ -29,28 +35,45 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         navController = Navigation.findNavController(view)
 
+        dataResult()
+        bottomItem()
+    }
+
+    fun dataResult(){
         //get Name From Login with bundle
         val hasil ="Welcome " + arguments?.getString("username")
         Log.d("Masuk", "onViewCreated:")
         user_name.text = hasil.toString()
+    }
 
+    fun bottomItem(){
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
 
-        btn_transaction.setOnClickListener {
-//            var bundle = bundleOf("angka" to 100)
-//            navController.navigate(R.id.action_homeFragment_to_transactionFragment,bundle)
-            navController.navigate(R.id.action_homeFragment_to_transactionFragment)
-        }
-
-        btn_blance.setOnClickListener {
-           navController.navigate(R.id.action_homeFragment_to_balanceFragment)
-        }
-
-        btn_history.setOnClickListener {
-            navController.navigate(R.id.action_homeFragment_to_historyFragment)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.btn_homeFragment -> {
+                    navController.navigate(R.id.action_loginFragment_to_homeFragment)
+                    true
+                }
+                R.id.btn_transactionFragment -> {
+                    navController.navigate(R.id.action_homeFragment_to_transactionFragment)
+                    true
+                }
+                R.id.btn_balanceFragment -> {
+                    navController.navigate(R.id.action_homeFragment_to_balanceFragment)
+                    true
+                }
+                else ->{
+                    println("Else")
+                    false
+                }
+            }
         }
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d("hancur_home", "onDestroy: ")
